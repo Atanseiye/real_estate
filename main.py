000061a0d8347f8ca2c63f169fcfd39bf0cb030b
Kolade <koladeatanseiye@gmail.com>
2025-01-20 10:12:34 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from process import wrangle, prediction
+import pandas as pd
+
+
+st.title("Real Estate Value Prediction App")
+
+# Example options for the dropdown
+type_option = ['Mini Flat', 'Bungalow', 'Terrace', 'Detached house', 'Semi-detached house']
+type_of_house = st.selectbox('Select an option:', type_option)
+
+# Boys Quarters
+boys_option = ['Present', 'Absent']
+boys_quarters = st.selectbox('Is Boy\'s quarters Available there?', boys_option)
+
+# Density
+density_option = ['High density', 'Medium density', 'Low density']
+density = st.selectbox('What is the desity of the location', density_option)
+
+condition_option = ['Very good', 'Good', 'Fair', 'Poor']
+condition = st.selectbox('What is the condition of the place', condition_option)
+
+# Ceiling
+ceiling_optioin = ['Asbestos', 'Others', 'POP', 'PVC']
+ceiling = st.selectbox('What type of ceiling is used', ceiling_optioin)
+
+# Floor type
+floor_option = ['Tiles', 'Others', 'Wooden', 'Marble', 'Cement screed']
+floor = st.selectbox('What type of Floor is used', floor_option)
+
+# Roof
+roof_option = ['Corrugated Iron Sheet', 'Long Span Aluminium Roofing sheet', 'Aluminium Step Tiles Roofing Sheet', 'Others']
+roof = st.selectbox('What type of roof is there', roof_option)
+
+# Window
+window_type = ['Glazed Aluminium Sliding Window', 'Casement', 'Others', 'Louvers Blade']
+window = st.selectbox('What is thw window type', window_type)
+
+# Painting
+painting_type = ['Textcote Paint', 'Emulsion Paint', 'Gloss Paint', 'Satin Paint']
+painting = st.selectbox('What type of paint is used', painting_type)
+
+# Water
+water_condition = ['Good', 'Fair', 'Bad']
+water = st.selectbox('What is the water condition', water_condition)
+
+# Electericity
+electericity_condition = ['Very good', 'Good', 'Fair', 'Bad']
+electricity = st.selectbox('What is the condition of the electricity', electericity_condition)
+
+# Car park
+car_park_available = ['Present', 'Not present']
+is_carPark = st.selectbox('Car park avalability', car_park_available)
+
+# Security
+security_option = ['Gated estate', 'Private gate/fence', 'Street gate']
+security = st.selectbox('What type of security is available', security_option)
+
+building_size = st.text_input('What is the size of the building')
+bedrooms = st.text_input('How many bedrooms are there')
+age = st.text_input('What is the age of the property')
+bathrooms = st.text_input('How many bathrooms are there')
+toilets = st.text_input('How many toilets are there')
+
+# ################################
+#####  Preprocesses ###############
+if st.button('Submit'):
+    input_data = {
+        'building_size': [building_size],
+        'type': [type_of_house],
+        'bedroom':[bedrooms],
+        'age':[age],
+        'bathrooms':[bathrooms],
+        'toilets': [toilets],
+        "Boy'squarter": [boys_quarters],
+        'loc_density': [density],
+        'condition': [condition],
+        'Ceiling ': [ceiling],
+        'Floor':[floor],
+        'Electricity': [electricity],
+        'Car_park': [is_carPark],
+        'Security': [security],
+        'Roof':[roof],
+        'Painting': [painting],
+        'Window ':[window],
+        'water':[water]
+    }
+
+    # data = pd.DataFrame(input_data, index=False)
+    data = pd.DataFrame(input_data)
+    # data = data[list(input_data.keys())]
+    proc_data = wrangle(data)
+    prediction = prediction(proc_data)[0]
+    st.write(f'Given the data you have inputed, I\'m able to say that the value for this house is #{prediction[0]}')
+    # print(data)
+
+
+
---
+++ process.py
@@ -0,0 +1,75 @@
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+import pickle as pkl
+import joblib
+import numpy as np
+
+
+def wrangle(new_data_df):  
+
+    
+    # Loading the saved encoders and scalers
+    type_encoder = joblib.load('type_encoder.pkl')
+    boy_encoder = joblib.load('boy_encoder.pkl')
+    density_encoder = joblib.load('density_encoder.pkl')
+    cond_encoder = joblib.load('cond_encoder.pkl')
+    ceil_encoder = joblib.load('ceil_encoder.pkl')
+    floor_encoder = joblib.load('floor_encoder.pkl')
+    roof_encoder = joblib.load('roof_encoder.pkl')
+    win_encoder = joblib.load('win_encoder.pkl')
+    paint_encoder = joblib.load('paint_encoder.pkl')
+    water_encoder = joblib.load('water_encoder.pkl')
+    ele_encoder = joblib.load('ele_encoder.pkl')
+    car_encoder = joblib.load('car_encoder.pkl')
+    sec_encoder = joblib.load('sec_encoder.pkl')
+
+    build_scaler = joblib.load('build_scaler.pkl')
+    bed_scaler = joblib.load('bed_scaler.pkl')
+    age_scaler = joblib.load('age_scaler.pkl')
+    toil_scaler = joblib.load('toil_scaler.pkl')
+    bath_scaler = joblib.load('bath_scaler.pkl')
+    scaler_y = joblib.load('scaler_y.pkl')
+    
+    # Apply the encoders and scalers to new data
+    new_data_df['type'] = type_encoder.transform(new_data_df['type'])
+    new_data_df["Boy'squarter"] = boy_encoder.transform(new_data_df["Boy'squarter"])
+    new_data_df['loc_density'] = density_encoder.transform(new_data_df['loc_density'])
+    new_data_df['condition'] = cond_encoder.transform(new_data_df['condition'])
+    new_data_df['Ceiling '] = ceil_encoder.transform(new_data_df['Ceiling '])
+    new_data_df['Floor'] = floor_encoder.transform(new_data_df['Floor'])
+    new_data_df['Roof'] = roof_encoder.transform(new_data_df['Roof'])
+    new_data_df['Window '] = win_encoder.transform(new_data_df['Window '])
+    new_data_df['Painting'] = paint_encoder.transform(new_data_df['Painting'])
+    new_data_df['water'] = water_encoder.transform(new_data_df['water'])
+    new_data_df['Electricity'] = ele_encoder.transform(new_data_df['Electricity'])
+    new_data_df['Car_park'] = car_encoder.transform(new_data_df['Car_park'])
+    new_data_df['Security'] = sec_encoder.transform(new_data_df['Security'])
+
+    new_data_df['building_size'] = build_scaler.transform(np.array(new_data_df['building_size']).reshape(-1, 1))
+    new_data_df['bedroom'] = bed_scaler.transform(np.array(new_data_df['bedroom']).reshape(-1, 1))
+    new_data_df['age'] = age_scaler.transform(np.array(new_data_df['age']).reshape(-1, 1))
+    new_data_df['toilets'] = toil_scaler.transform(np.array(new_data_df['toilets']).reshape(-1, 1))
+    new_data_df['bathrooms'] = bath_scaler.transform(np.array(new_data_df['bathrooms']).reshape(-1, 1))
+
+    
+    return new_data_df
+
+
+
+
+
+def prediction(inputs):
+
+        
+    with open('model.pkl', 'rb') as f:
+        model = pkl.load(f)
+
+    scaler_y = joblib.load('scaler_y.pkl')
+        
+        
+    inputs = inputs.values.reshape(1, -1)
+    
+    predict = model.predict(inputs)
+    
+    trasform = scaler_y.inverse_transform(predict.reshape(-1, 1))
+    
+    return trasform
